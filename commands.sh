8103353ea8ba9304de9d4c964a00d4362a8d821f
Indu Admin <indu.admin@Anils-MBP.fios-router.home>
2022-06-13 21:48:04 -0400

    Removed picture

--- commands.sh
+++ commands.sh
@@ -8,3 +8,18 @@ Heroku setup:
 Found an SSH public key at /Users/indu.admin/.ssh/id_rsa.pub
 Would you like to upload it to Heroku? Yes
 Uploading /Users/indu.admin/.ssh/id_rsa.pub SSH key... done
+
+
+>heroku create komma-weather-application
+
+Creating ⬢ komma-weather-application... done
+https://komma-weather-application.herokuapp.com/ | https://git.heroku.com/komma-weather-application.git
+
+> In package.json
+
+  "scripts": {
+    "start" : "node src/app.js" 
+  },
+
+> Push all the code to heroku
+indu.admin@Anils-MBP web-server % git push heroku main 
